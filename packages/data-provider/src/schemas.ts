136eeb55e3f02f055b75824f10b15d4d1b610f1f
Ricardo Martins <RicardoMartins@criticalmanufacturing.com>
2026-01-26 10:53:56 +0000

    feat: Set MCPServers with URL parameters

--- packages/data-provider/src/schemas.ts
+++ packages/data-provider/src/schemas.ts
@@ -1004,6 +1004,7 @@ export const tPresetSchema = tConversationSchema
       defaultPreset: z.boolean().optional(),
       order: z.number().optional(),
       endpoint: extendedModelEndpointSchema.nullable(),
+      mcp: z.string().optional(),
     }),
   );
 
